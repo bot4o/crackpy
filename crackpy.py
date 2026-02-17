9e70506bb596c4ade654c045f372c88657bbd7c6
bot4o <boevaleksandar@gmail.com>
2026-02-17 22:37:10 +0200

    some pseudo

--- crackpy.py
+++ crackpy.py
@@ -1,1 +1,13 @@
-print("bruh")
+# Takes a target hash (MD5 or SHA-256)
+
+# Takes a hash algorithm name
+
+# Takes a wordlist file path
+
+# Tries every word from the list -> computer hash -> compares
+
+# Prints match
+
+# If otherwise, says nothing found
+
+# Shows progress / stats (optional but nice)
